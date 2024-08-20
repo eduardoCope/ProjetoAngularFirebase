42329689a9c090e97730345041fa621e582fbce3
Eduardo Pellegrino <162513336+eduardoCope@users.noreply.github.com>
2024-08-20 22:34:32 +0000

    color

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,76 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
+  segments: string = 'usuario';
 
+  usuario = [
+    {
+      nome: 'Jose Emersson',
+      idade: '20',
+      genero: 'masculino',
+      rotina: [
+        {
+          dia: 'Segunda-feira',
+          treinos: [
+            'Rosca Direta',
+            'Crucifixo',
+            'Remada sumo'
+          ] 
+        },
+        {
+          dia: 'Quarta-feira',
+          treinos: [
+            'Rosca Invertida',
+            'Crucifixo',
+            'Abdominal'
+          ] 
+        },
+        {
+          dia: 'Sexta-feira',
+          treinos: [
+            'Supino Inclinado',
+            'Biceps Corda',
+            'Hulk invertido'
+          ] 
+        }
+      ]
+    },
+    {
+      nome: 'julia Valadao',
+      idade: '17',
+      genero: 'feminino',
+      rotina: [
+        {
+          dia: 'Terca-feira',
+          treinos: [
+            'Prancha',
+            'Agachamento',
+            'Abdominal'
+          ] 
+        },
+        {
+          dia: 'Quinta-feira',
+          treinos: [
+            'Flexao',
+            'Cardio',
+            'Agachamento'
+          ] 
+        },
+        {
+          dia: 'Sabado',
+          treinos: [
+            'Supino Inclinado',
+            'Abdominal',
+            'Cardio'
+          ] 
+        }
+      ]
+    }
+  ]
+
+constructor(){}
+
+trocar(event: any){
+  this.segments = event.detail.value;
+}
 }
